18d33c623fa61510e0ab9091dffb5b51b104c122
bahrienis <bahrienisdemirtel09@hotmail.com>
2017-11-18 20:47:41 +0100

    last changes

--- src/lane_detection/src/Method3.cpp
+++ src/lane_detection/src/Method3.cpp
@@ -31,11 +31,11 @@
 
 using namespace std;
 
-int heightofframe = 240;
-int widthofframe = 320;
+int heightofframe = 480;
+int widthofframe = 640;
 int degreeofthepolynom = 2;
 int fpsvalue = 30;
-double thresholdvalue = 0.8;
+double thresholdvalue = 0.9;
 double r[3];
 int firstpicsize = (2 * heightofframe / 3);
 
@@ -219,7 +219,7 @@ int main(int argc, char **argv) {
 
         //std::string filename = "/home/enis/Desktop/Masterarbeit/photos_31.08.2017_geradeaus/frame187.jpg";
         //  std::string filename = "/home/enis/Desktop/Masterarbeit/photos_04.09.2017/frame9.jpg";
-        //      std::string filename = "/home/enis/Desktop/Masterarbeit/photos_04.09.2017/frame152.jpg";
+    //          std::string filename = "/home/enis/Desktop/Masterarbeit/photos_04.09.2017/frame152.jpg";
 
         //std::string filename = "/home/enis/Desktop/Masterarbeit/deneme2/frame12.jpg";
         //std::string filename = "/home/enis/Desktop/Masterarbeit/frame0058.jpg";
@@ -227,12 +227,12 @@ int main(int argc, char **argv) {
         //std::string filename = "/home/enis/Desktop/Masterarbeit/photos_31.08.2017_lighton/frame97.jpg";
 
 
-        /*       inputImg = imread(filename, CV_LOAD_IMAGE_COLOR);
+      /*         inputImg = imread(filename, CV_LOAD_IMAGE_COLOR);
                 if (inputImg.empty()) {
                     cout << "can not open " << filename << endl;
                     return -1;
                 }
-         */
+      */   
         printf("FRAME #%6d ", frameNum);
         fflush(stdout);
         frameNum++;
